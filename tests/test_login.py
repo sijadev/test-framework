a35afea19f67a5e3096e9a7a018a221bc85ce0a4
sija <79046057+sijadev@users.noreply.github.com>
2025-07-24 12:04:03 +0200

    delete all files

--- tests/test_login.py
+++
@@ -1,9 +0,0 @@
-
-from typing import Any
-from selenium.webdriver.safari.webdriver import WebDriver
-from pages.login_page import LoginPage
-
-def test_login_success(browser: WebDriver, test_data: Any):
-    page = LoginPage(browser)
-    page.login(test_data["username"], test_data["password"])
-    assert "Log in to the site | New Site" in browser.title
